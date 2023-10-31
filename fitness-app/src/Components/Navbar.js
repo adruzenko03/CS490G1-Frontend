3439549134634e8cf1e7a13f3f297894071598a2
Ja Xian Chen <jxc4@njit.edu>
2023-10-31 19:11:07 -0400

    pages added

--- fitness-app/src/Components/Navbar.js
+++ fitness-app/src/Components/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Navbar.css';
 import Footer from '../Components/Footer';
+import {Link} from "react-router-dom"
+
 function Navbar() {
   return (
     <div className="navbar">
-      <div className="navbar-top">
-        <div className='navbar-item'>HOME</div>
-        <div className='navbar-item'>WORKOUTS</div>
-        <div className='navbar-item'>COACHES</div>
+      <div className="links">
+        <Link to='./'>HOME</Link>
+        <Link to='/Workouts'>WORKOUTS</Link>
+        <Link to='Coaches'>COACHES</Link>
       </div>
       <Footer />
     </div>
--- fitness-app/src/Components/QuoteSection.js
+++ fitness-app/src/Components/QuoteSection.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import './QuoteSection.css';
-function QuoteSection() {
-  return (
+import React, { Component } from "react";
+import "./QuoteSection.css";
+
+export class QuoteSection extends Component {
+  render() {
+    return (
     <div className="quote-section">
-      <h2>“IF YOU DON’T FIND THE TIME, IF YOU DON’T DO THE <br></br> WORK, YOU DON’T GET THE RESULTS.”<br></br>— ARNOLD SCHWARZENEGGER</h2>
+      <h2>
+        “IF YOU DON’T FIND THE TIME, IF YOU DON’T DO THE <br></br> WORK, YOU
+        DON’T GET THE RESULTS.”<br></br>— ARNOLD SCHWARZENEGGER
+      </h2>
     </div>
-  );
+  );}
 }
 
 export default QuoteSection;
