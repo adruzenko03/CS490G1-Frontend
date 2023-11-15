d2e94ea1000de29042c2fa10780acf4ca995518f
Ja Xian Chen <jxc4@njit.edu>
2023-11-15 16:52:33 -0500

    useStates added

--- fitness-app/src/Components/ActivityForm.js
+++ fitness-app/src/Components/ActivityForm.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import React, { useState } from "react";
+
+  const [calorieIntake, setCalorieIntake] = useState("");
+  const [proteinIntake, setProteinIntake] = useState("");
+  const [waterIntake, setWaterIntake] = useState("");
+  const [exercise, setExercise] = useState("");
+  const [mood, setMood] = useState("");
+  const [weight, setWeight] = useState("");
+
+  const handleSave = () => {
+    console.log("Form data saved:", {
+      calorieIntake,
+      proteinIntake,
+      waterIntake,
+      exercise,
+      mood,
+      weight,
+    });
+  };
+
 
 export default function ActivityForm() {
   return (
-    <div>ActivityForm</div>
+    <div className='form-container'>Activity Form</div>
   )
 }
--- fitness-app/src/pages/Activity.jsx
+++ fitness-app/src/pages/Activity.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./styles/Activity.css";
+import ActivityForm from "../Components/ActivityForm";
 
 export default function Activity() {
   return (
     <div className="activity-page">
       <div className="header">Your Daily Activity</div>
+      <ActivityForm></ActivityForm>
     </div>
   );
 }
