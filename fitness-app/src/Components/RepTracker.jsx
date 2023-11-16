3ff4deb627273a6270fbb400c20b9ab690cc5ad5
Ja Xian Chen <jxc4@njit.edu>
2023-11-16 12:03:41 -0500

    rep tracker added

---
+++ fitness-app/src/Components/RepTracker.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+
+const RepTracker = () => {
+  const [exercise, setExercise] = useState('');
+  const [sets, setSets] = useState([{ reps: '', weight: '' }]);
+
+  const handleAddSet = () => {
+    setSets([...sets, { reps: '', weight: '' }]);
+  };
+
+  const handleDeleteSet = (index) => {
+    const newSets = [...sets];
+    newSets.splice(index, 1);
+    setSets(newSets);
+  };
+
+  const handleInputChange = (index, key, value) => {
+    const newSets = [...sets];
+    newSets[index][key] = value;
+    setSets(newSets);
+  };
+
+  const handleExerciseChange = (value) => {
+    setExercise(value);
+  };
+
+  return (
+    <div>      
+        <h2 className='header'>Rep Tracker</h2>
+    <div className='form-container'>
+      <label>
+        Exercise:
+        <input
+          type="text"
+          value={exercise}
+          onChange={(e) => handleExerciseChange(e.target.value)}
+        />
+      </label>
+      {sets.map((set, index) => (
+        <div key={index}>
+          <label>
+            Reps:
+            <input
+              type="number"
+              value={set.reps}
+              onChange={(e) => handleInputChange(index, 'reps', e.target.value)}
+            />
+          </label>
+          <label>
+            Weight:
+            <input
+              type="number"
+              value={set.weight}
+              onChange={(e) => handleInputChange(index, 'weight', e.target.value)}
+            />
+          </label>
+          {sets.length > 1 && (
+            <button type="button" onClick={() => handleDeleteSet(index)}>
+              Delete
+            </button>
+          )}
+        </div>
+      ))}
+      <button type="button" onClick={handleAddSet}>
+        Add Set
+      </button>
+    </div></div>
+  );
+};
+
+export default RepTracker;
--- fitness-app/src/pages/Activity.jsx
+++ fitness-app/src/pages/Activity.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./styles/Activity.css";
 import ActivityForm from "../Components/ActivityForm";
+import RepTracker from "../Components/RepTracker";
 
 export default function Activity() {
   return (
     <div className="activity-page">
       <div className="header">Your Daily Activity</div>
       <ActivityForm></ActivityForm>
+      <RepTracker></RepTracker>
     </div>
   );
 }
