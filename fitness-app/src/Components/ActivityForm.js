eca2e4df4a8aefa6f234e7f8fe85a1c278d18383
Ja Xian Chen <jxc4@njit.edu>
2023-11-15 20:19:40 -0500

    activity form

--- fitness-app/src/Components/ActivityForm.js
+++ fitness-app/src/Components/ActivityForm.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 
+export default function ActivityForm() {
   const [calorieIntake, setCalorieIntake] = useState("");
   const [proteinIntake, setProteinIntake] = useState("");
   const [waterIntake, setWaterIntake] = useState("");
@@ -17,11 +18,77 @@ import React, { useState } from "react";
       mood,
       weight,
     });
+    setCalorieIntake("");
+    setProteinIntake("");
+    setWaterIntake("");
+    setExercise("");
+    setMood("");
+    setWeight("");
   };
 
-
-export default function ActivityForm() {
   return (
-    <div className='form-container'>Activity Form</div>
-  )
+    <div className="form-container">
+      <div className="title">Date</div>
+      <form className="activity-form">
+        <label>
+          Calorie Intake:
+          <input
+            type="text"
+            value={calorieIntake}
+            onChange={(e) => setCalorieIntake(e.target.value)}
+          />
+        </label>
+
+        <label>
+          Protein Intake:
+          <input
+            type="text"
+            value={proteinIntake}
+            onChange={(e) => setProteinIntake(e.target.value)}
+          />
+        </label>
+
+        <label>
+          Water Intake:
+          <input
+            type="text"
+            value={waterIntake}
+            onChange={(e) => setWaterIntake(e.target.value)}
+          />
+        </label>
+
+        <label>
+          Exercise:
+          <input
+            type="text"
+            value={exercise}
+            onChange={(e) => setExercise(e.target.value)}
+          />
+        </label>
+
+        <label>
+          Mood:
+          <select value={mood} onChange={(e) => setMood(e.target.value)}>
+            <option value=""></option>
+            <option value="happy">Happy</option>
+            <option value="neutral">Neutral</option>
+            <option value="sad">Sad</option>
+          </select>
+        </label>
+
+        <label>
+          Weight:
+          <input
+            type="text"
+            value={weight}
+            onChange={(e) => setWeight(e.target.value)}
+          />
+        </label>
+
+        <button type="button" onClick={handleSave}>
+          Save
+        </button>
+      </form>
+    </div>
+  );
 }
