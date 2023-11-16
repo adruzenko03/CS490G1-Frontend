0719c2b6b361e75d2ec273cbd9cc9c1013f0a531
Ja Xian Chen <jxc4@njit.edu>
2023-11-16 16:32:50 -0500

    progress page

--- fitness-app/src/pages/Progress.jsx
+++ fitness-app/src/pages/Progress.jsx
@@ -5,6 +5,8 @@ export default function Progress() {
   return (
     <div className="progress-page">
       <div className="header">Progress</div>
+      <div className="form-container">Daily Calorie Intake</div>
+      <div className="form-container">Exercise</div>
     </div>
   );
 }
