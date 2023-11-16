b57c090a07295c786ad5ea8079dbf97d15d49c79
gleni1 <glenipoleshi@gmail.com>
2023-11-15 20:32:23 -0500

    coaches page setup

---
+++ CS490G1-Frontend/fitness-app/src/Components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Navbar.css';
+import Footer from './Footer';
+import { Link } from "react-router-dom"
+
+function Navbar({ onLoginClick, onSignupClick, userRole }) {
+  return (
+    <div className="navbar">
+      <div className="links">
+        <Link to='./'>TRU-FIT</Link>
+        <Link to='/Workouts'>WORKOUTS</Link>
+        <Link to='Coaches'>COACHES</Link>
+      </div>
+      <Footer onLoginClick={onLoginClick} onSignupClick={onSignupClick} userRole={userRole}/>
+    </div>
+  );
+}
+
+export default Navbar;
