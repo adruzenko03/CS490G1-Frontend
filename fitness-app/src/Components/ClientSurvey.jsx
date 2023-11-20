3791622b93a4bad82cd9513638dec4d530406565
Ja Xian Chen <jxc4@njit.edu>
2023-11-20 18:24:10 -0500

    renamed files

--- fitness-app/src/Components/ClientSurvey.jsx
+++ fitness-app/src/Components/ClientSurvey.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import './clientsurvey.css'
+import './styles/clientsurvey.css'
 
 function ClientSurvey({ onClose }){
     const [surveyData, setSurveyData] = useState({
