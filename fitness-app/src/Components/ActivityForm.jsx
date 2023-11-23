6cd933c3126bfa3a18710bffdc140562062990d7
Ja Xian Chen <jxc4@njit.edu>
2023-11-22 20:49:59 -0500

    added error on wrong format inputs

--- fitness-app/src/Components/ActivityForm.jsx
+++ fitness-app/src/Components/ActivityForm.jsx
@@ -4,6 +4,7 @@ export default function ActivityForm() {
   const [calorieIntake, setCalorieIntake] = useState("");
   const [weight, setWeight] = useState("");
   const [formSaved, setFormSaved] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
   const currentDate = getFormattedDate();
 
   function getFormattedDate() {
@@ -12,18 +13,40 @@ export default function ActivityForm() {
   }
 
   const handleSave = () => {
+    if (!isValidInput(calorieIntake) || !isValidInput(weight)) {
+      setErrorMessage("Please enter valid numbers.");
+      return;
+    }
+
     console.log("Form data saved:", {
       date: getFormattedDate(),
-      calorieIntake,
-      weight,
+      calorieIntake: parseFloat(calorieIntake),
+      weight: parseFloat(weight),
     });
     setCalorieIntake("");
     setWeight("");
     setFormSaved(true);
+    setErrorMessage("");
   };
 
   const handleReset = () => {
     setFormSaved(false);
+  };
+
+  const handleInputChange = (e, setValue) => {
+    const inputValue = e.target.value;
+    if (!isValidInput(inputValue)) {
+      setErrorMessage("Please enter a valid number.");
+    } else {
+      setErrorMessage("");
+    }
+    setValue(inputValue);
+  };
+
+  const isValidInput = (value) => {
+    // Use regex to check if the input is a valid number (integer or decimal)
+    const numberRegex = /^-?\d+(\.\d*)?$/;
+    return numberRegex.test(value);
   };
 
   return (
@@ -34,13 +57,13 @@ export default function ActivityForm() {
         </div>
       ) : (
         <form className="activity-form">
-          <div className="title"> {currentDate}</div>
+          <div className="title">{currentDate}</div>
           <label>
             Calorie Intake:
             <input
               type="text"
               value={calorieIntake}
-              onChange={(e) => setCalorieIntake(e.target.value)}
+              onChange={(e) => handleInputChange(e, setCalorieIntake)}
             />
           </label>
 
@@ -49,9 +72,11 @@ export default function ActivityForm() {
             <input
               type="text"
               value={weight}
-              onChange={(e) => setWeight(e.target.value)}
+              onChange={(e) => handleInputChange(e, setWeight)}
             />
           </label>
+
+          {errorMessage && <div className="error-message">{errorMessage}</div>}
 
           <button type="button" onClick={handleSave}>
             Save
