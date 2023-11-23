37575ca20ad17e4f6500e8a7def059e39719cb08
Ja Xian Chen <jxc4@njit.edu>
2023-11-22 20:42:46 -0500

    check in

--- fitness-app/src/Components/ActivityForm.jsx
+++ fitness-app/src/Components/ActivityForm.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 export default function ActivityForm() {
   const [calorieIntake, setCalorieIntake] = useState("");
   const [weight, setWeight] = useState("");
+  const [formSaved, setFormSaved] = useState(false);
   const currentDate = getFormattedDate();
 
   function getFormattedDate() {
@@ -12,40 +13,58 @@ export default function ActivityForm() {
 
   const handleSave = () => {
     console.log("Form data saved:", {
-      date: currentDate,
+      date: getFormattedDate(),
       calorieIntake,
       weight,
     });
     setCalorieIntake("");
     setWeight("");
+    setFormSaved(true);
+  };
+
+  const handleReset = () => {
+    setFormSaved(false);
   };
 
   return (
     <div className="form-container">
-      <div className="title"> {currentDate}</div>
-      <form className="activity-form">
-        <label>
-          Calorie Intake:
-          <input
-            type="text"
-            value={calorieIntake}
-            onChange={(e) => setCalorieIntake(e.target.value)}
-          />
-        </label>
+      {formSaved ? (
+        <div className="check-in-message">
+          Check-in completed for {getFormattedDate()}
+        </div>
+      ) : (
+        <form className="activity-form">
+          <div className="title"> {currentDate}</div>
+          <label>
+            Calorie Intake:
+            <input
+              type="text"
+              value={calorieIntake}
+              onChange={(e) => setCalorieIntake(e.target.value)}
+            />
+          </label>
 
-        <label>
-          Weight:
-          <input
-            type="text"
-            value={weight}
-            onChange={(e) => setWeight(e.target.value)}
-          />
-        </label>
+          <label>
+            Weight:
+            <input
+              type="text"
+              value={weight}
+              onChange={(e) => setWeight(e.target.value)}
+            />
+          </label>
 
-        <button type="button" onClick={handleSave}>
-          Save
-        </button>
-      </form>
+          <button type="button" onClick={handleSave}>
+            Save
+          </button>
+        </form>
+      )}
+      <div className="activity-form">
+        {formSaved && (
+          <button type="button" onClick={handleReset}>
+            Edit Check-in
+          </button>
+        )}
+      </div>
     </div>
   );
 }
