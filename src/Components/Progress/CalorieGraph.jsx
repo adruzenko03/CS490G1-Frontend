d2c9fd83abc39d32d6d3efee572e1bdd07c517e1
Ja Xian Chen <jxc4@njit.edu>
2023-12-08 20:55:00 -0500

    fixed graph drop down

--- src/Components/Progress/CalorieGraph.jsx
+++ src/Components/Progress/CalorieGraph.jsx
@@ -10,33 +10,31 @@ import {
 } from "recharts";
 
 const CalorieGraph = ({ data }) => {
-  const [selectedYear, setSelectedYear] = useState(null);
   const [selectedMonth, setSelectedMonth] = useState(null);
 
-  // Extract unique years and months from the data
-  const uniqueYears = [...new Set(data.map((item) => item.Date.slice(0, 4)))];
-  const uniqueMonths = [...new Set(data.map((item) => item.Date.slice(5, 7)))];
+  // Extract unique months and years from the data
+  const uniqueMonths = [
+    ...new Set(
+      data.map((item) => {
+        const date = new Date(item.Date);
+        return date.toLocaleDateString("en-US", {
+          month: "short",
+          year: "numeric",
+        });
+      })
+    ),
+  ];
 
-  // Filter data based on selected year and month
-  const filteredData = data.filter((item) => {
-    return (
-      (!selectedYear || item.Date.startsWith(selectedYear)) &&
-      (!selectedMonth || item.Date.slice(5, 7) === selectedMonth)
-    );
-  });
+  // Sort months in chronological order
+  uniqueMonths.sort();
+
+  // Filter data based on selected month
+  const filteredData = selectedMonth
+    ? data.filter((item) => item.Date.startsWith(selectedMonth))
+    : data;
 
   return (
     <div>
-      <label>Select Year: </label>
-      <select onChange={(e) => setSelectedYear(e.target.value)}>
-        <option value="">All Years</option>
-        {uniqueYears.map((year, index) => (
-          <option key={index} value={year}>
-            {year}
-          </option>
-        ))}
-      </select>
-
       <label>Select Month: </label>
       <select onChange={(e) => setSelectedMonth(e.target.value)}>
         <option value="">All Months</option>
