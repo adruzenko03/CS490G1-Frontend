74ee347a9fe77bddc92a648203b770792577b515
Ja Xian Chen <jxc4@njit.edu>
2023-12-16 21:37:21 -0500

    x axis labeled

--- src/Components/Progress/MoodGraph.jsx
+++ src/Components/Progress/MoodGraph.jsx
@@ -63,7 +63,13 @@ const MoodGraph = ({ data }) => {
 
       <LineChart width={600} height={300} data={filteredData}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="Date" />
+        <XAxis
+          dataKey="entry_date"
+          tickFormatter={(entry_date) => {
+            const date = new Date(entry_date);
+            return `${date.getMonth() + 1}/${date.getDate()}`;
+          }}
+        />
         <YAxis ticks={ticks} />
         <Tooltip
           formatter={(value, name, props) => {
