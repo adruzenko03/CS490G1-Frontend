e8ed0cfc8a3a0c3dcfc5d059d5272a7f29201925
Alex <adruzenko03@gmail.com>
2023-12-20 21:43:05 -0500

    another fix

--- src/Components/MyClients/ClientRequests.jsx
+++ src/Components/MyClients/ClientRequests.jsx
@@ -9,7 +9,7 @@ import axios from 'axios';
 const ClientRequests = ({userId}) => {
   const [clientsList, setClientsList] = useState([]);
 
-  const coachId = localStorage.getItem("userId");
+  const coachId = userId;
 
 
   useEffect(() => {
